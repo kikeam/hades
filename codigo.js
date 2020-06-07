4aaa6ef42c6643707cc4622b88e53f5711f946bf
kikeam <66570956+kikeam@users.noreply.github.com>
2020-06-07 21:41:01 +0200

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,12 @@
+/* let num1 =1;
+let num2 =5;
+
+if (num>0) {
+    console.log("num es mayor que cero");
+    console.log(`${num} es mayor que cero`);
+} else if (num!=0){
+    console.log("num es menor que cero");
+    console.log(`${num} es menor que cero`);
+} else console.log("el numero es cero") 
+
+if ((num1>0)&&(num2>0)) console.log("uno de los numeros es mayor que 0"); */
